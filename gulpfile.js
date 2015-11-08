781786eb3382560f83d60caf4844d7dc482a3398
Tobias Reich <tobias.reich.ich@gmail.com>
2015-11-08 16:28:59 +0100

    Simplified gulp-file using basicTasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,16 @@
 'use strict'
 
-let name       = require('./package.json').moduleName,
-    gulp       = require('gulp'),
-    browserify = require('browserify'),
-    babelify   = require('babelify'),
-    source     = require('vinyl-source-stream'),
-    buffer     = require('vinyl-buffer'),
-    plugins    = require('gulp-load-plugins')()
+let name  = require('./package.json').moduleName,
+    gulp  = require('gulp'),
+    tasks = require('@electerious/basictasks')(gulp, name)
 
-const catchError = function(err) {
-
-	console.log(err.toString())
-	this.emit('end')
-
-}
-
-const scripts = function() {
-
-	let bify = browserify({
-		entries    : './src/scripts/main.js',
-		standalone : name
-	})
-
-	let transformer = babelify.configure({
-		presets: ['es2015']
-	})
-
-	bify.transform(transformer)
-	    .bundle()
-	    .on('error', catchError)
-	    .pipe(source(name + '.min.js'))
-	    .pipe(buffer())
-	    .pipe(plugins.uglify())
-	    .on('error', catchError)
-	    .pipe(gulp.dest('./dist'))
-
-}
+const scripts = tasks.scripts({
+	from : './src/scripts/main.js',
+	to   : './dist'
+})
 
 const watch = function() {
-
-	gulp.watch('./src/styles/**/*.scss', ['styles'])
 	gulp.watch('./src/scripts/**/*.js', ['scripts'])
-
 }
 
 gulp.task('scripts', scripts)
